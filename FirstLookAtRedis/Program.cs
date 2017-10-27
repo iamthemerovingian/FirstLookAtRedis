53313582f923d7047ff179e060b90a56131d68bc
MilindaSeneviratne <milinda.seneviratne@chilindo.com>
2017-10-27 13:33:23 +0700

    Basic Hello World steps.

--- FirstLookAtRedis/Program.cs
+++ FirstLookAtRedis/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceStack.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace FirstLookAtRedis
     {
         static void Main(string[] args)
         {
+            using (IRedisNativeClient client = new RedisClient(new RedisEndpoint { Host = "117.20.40.28", Port = 6379, Password = "Yellow889" }))
+            {
+                client.Set("urn:messages:1", Encoding.UTF8.GetBytes("Hello C# World"));
+            }
+
+            using (IRedisNativeClient client = new RedisClient())
+            {
+                string result = Encoding.UTF8.GetString(client.Get("urn:messages:1"));
+
+                Console.WriteLine($"Mesage: {result}");
+                Console.ReadLine();
+            }
         }
     }
 }
