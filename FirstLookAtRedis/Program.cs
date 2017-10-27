c6fc75aad9fc924bf0abb0e61f0821a39d485e51
MilindaSeneviratne <milinda.seneviratne@chilindo.com>
2017-10-27 14:18:48 +0700

    Transactions in redis.

--- FirstLookAtRedis/Program.cs
+++ FirstLookAtRedis/Program.cs
@@ -47,33 +47,47 @@ namespace FirstLookAtRedis
 
             //}
 
-            long lastId = 0;
+            //long lastId = 0;
+
+            //using (IRedisClient client = new RedisClient(new RedisEndpoint { Host = "117.20.40.28", Port = 6379, Password = "Yellow889" }))
+            //{
+            //    var customerClient = client.As<Customer>();
+            //    var customer = new Customer
+            //    {
+            //        Id = customerClient.GetNextSequence(),
+            //        Address = "123 Main Street.",
+            //        Name = "Bob Green",
+            //        Orders = new List<Order> { new Order { OrderNumber = "AB 123" }, new Order { OrderNumber = "AB 456" } }
+            //    };
+
+            //    var storedCustomer = customerClient.Store(customer);
+            //    lastId = storedCustomer.Id;
+            //}
+
+            //using (IRedisClient client = new RedisClient(new RedisEndpoint { Host = "117.20.40.28", Port = 6379, Password = "Yellow889" }))
+            //{
+            //    var customerClient = client.As<Customer>();
+            //    var customer = customerClient.GetById(lastId);
+
+            //    Console.WriteLine($"Customer Name: {customer.Name}, Id: {customer.Id}, Address: {customer.Address}, Orders: ");
+            //    foreach (var item in customer.Orders)
+            //    {
+            //        Console.WriteLine(item.OrderNumber);
+            //    }
+            //}
 
             using (IRedisClient client = new RedisClient(new RedisEndpoint { Host = "117.20.40.28", Port = 6379, Password = "Yellow889" }))
             {
-                var customerClient = client.As<Customer>();
-                var customer = new Customer
-                {
-                    Id = customerClient.GetNextSequence(),
-                    Address = "123 Main Street.",
-                    Name = "Bob Green",
-                    Orders = new List<Order> { new Order { OrderNumber = "AB 123" }, new Order { OrderNumber = "AB 456" } }
-                };
-
-                var storedCustomer = customerClient.Store(customer);
-                lastId = storedCustomer.Id;
+                var transaction = client.CreateTransaction();
+                transaction.QueueCommand(c => c.Set("abc", 1));
+                transaction.QueueCommand(c => c.Increment("abc", 1));
+                transaction.Commit();
             }
 
             using (IRedisClient client = new RedisClient(new RedisEndpoint { Host = "117.20.40.28", Port = 6379, Password = "Yellow889" }))
             {
-                var customerClient = client.As<Customer>();
-                var customer = customerClient.GetById(lastId);
-
-                Console.WriteLine($"Customer Name: {customer.Name}, Id: {customer.Id}, Address: {customer.Address}, Orders: ");
-                foreach (var item in customer.Orders)
-                {
-                    Console.WriteLine(item.OrderNumber);
-                }
+                int result = client.Get<int>("abc");
+                Console.WriteLine(result);
             }
 
             Console.ReadLine();
